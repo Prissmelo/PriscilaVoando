7e29e0d88de28bd0725a9467fd109386632e55a2
Priscila Melo <87330679+Prissmelo@users.noreply.github.com>
2021-12-20 18:45:51 -0300

    Add files via upload

---
+++ Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using CRUDMVC.Enum;
+using System;
+
+namespace CRUDMVC.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF  { get; set; }
+       
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+        public Situacao Situacao { get; set; }
+        public DateTime Nascimento { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
